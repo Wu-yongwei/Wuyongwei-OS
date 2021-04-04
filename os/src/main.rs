040317118006e6687bca5bc68e127eef32076310
Wu Yongwei <65208655+Wu-yongwei@users.noreply.github.com>
2021-04-04 11:43:00 +0800

    动态内存分配

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,47 +1,71 @@
 //! # 全局属性
-//! - `#![no_std]` 
-//! 禁⽤标准库
+//! - `#![no_std]`
+//!   禁用标准库
 #![no_std]
 //!
-//! - `#![no_main]` 
-//! 不使⽤ `main` 函数等全部 Rust-level ⼊⼝点来作为程序⼊⼝
+//! - `#![no_main]`
+//!   不使用 `main` 函数等全部 Rust-level 入口点来作为程序入口
 #![no_main]
-//! # ⼀些 unstable 的功能需要在 crate 层级声明后才可以使⽤
-//! - `#![feature(llvm_asm)]` 
-//! 内嵌汇编
+//! # 一些 unstable 的功能需要在 crate 层级声明后才可以使用
+//! - `#![feature(llvm_asm)]`
+//!   内嵌汇编
 #![feature(llvm_asm)]
 //!
-//! - `#![feature(global_asm)]` 
-//! 内嵌整个汇编⽂件
+//! - `#![feature(global_asm)]`
+//!   内嵌整个汇编文件
 #![feature(global_asm)]
 //!
-//! - `#![feature(panic_info_message)]` 
-//! panic! 时，获取其中的信息并打印
+//! - `#![feature(panic_info_message)]`
+//!   panic! 时，获取其中的信息并打印
 #![feature(panic_info_message)]
+//!
+//! - `#![feature(alloc_error_handler)]`
+//!   我们使用了一个全局动态内存分配器，以实现原本标准库中的堆内存分配。
+//!   而语言要求我们同时实现一个错误回调，这里我们直接 panic
+#![feature(alloc_error_handler)]
+
 #[macro_use]
 mod console;
 mod panic;
 mod sbi;
+mod memory;
 mod interrupt;
-// 汇编编写的程序⼊⼝，具体⻅该⽂件
+
+extern crate alloc;
+
+use crate::sbi::*;
+
+
+// 汇编编写的程序入口，具体见该文件
 global_asm!(include_str!("entry.asm"));
-/// Rust 的⼊⼝函数
+
+/// Rust 的入口函数
 ///
-/// 在 `_start` 为我们进⾏了⼀系列准备之后，这是第⼀个被调⽤的 Rust 函数
+/// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    println!("Hello 吴咏蔚!");
     // 初始化各种模块
     interrupt::init();
-    
-    unsafe {
-        llvm_asm!("ebreak"::::"volatile");
-    };
-    
-    loop{};
-    
-    unreachable!();
-    
-    panic!("end of rust_main");
-    
+    memory::init();
+
+    // 动态内存分配测试
+    use alloc::boxed::Box;
+    use alloc::vec::Vec;
+    let v = Box::new(5);
+    assert_eq!(*v, 5);
+    core::mem::drop(v);
+
+    let mut vec = Vec::new();
+    for i in 0..10000 {
+        vec.push(i);
+    }
+    assert_eq!(vec.len(), 10000);
+    for (i, value) in vec.into_iter().enumerate() {
+        assert_eq!(value, i);
+    }
+    println!("heap test passed,吴咏蔚!");
+
+    println!("shutdown!");
+    sbi::shutdown();
+    //panic!()
 }
